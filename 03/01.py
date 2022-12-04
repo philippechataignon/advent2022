2bacb3b17d9a5f8283c298fbda5a4fedda24b690
Philippe Chataignon <philippe.chataignon@gmail.com>
2022-12-04 11:03:53 +0100

    Unify 03

--- 03/01.py
+++ 03/01.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 def score(c):
     if "a" <= c <= "z":
         return ord(c) - 96
@@ -9,14 +8,27 @@ def score(c):
 
 if __name__ == '__main__':
     f = open("input.txt")
-    cum = 0
+    cum1 = 0
     for ll in f:
         l = ll[:-1]
         m = len(l) // 2
         a, b = l[:m], l[m:]
         c = set(a).intersection(set(b)).pop()
-        cum += score(c)
-    print(cum)
+        cum1 += score(c)
+    print(cum1)
 
-
-
+    f = open("input.txt")
+    cum2 = 0
+    end = False
+    while True:
+        g = []
+        for n in range(3):
+            ll = f.readline()
+            if not ll:
+                end = True
+            g.append(ll[:-1])
+        if end:
+            break
+        c = set(g[0]).intersection(set(g[1])).intersection(set(g[2])).pop()
+        cum2 += score(c)
+    print(cum2)
