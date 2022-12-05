d75be0b365a3b6f554af3142bddf651d5dc7460f
Philippe Chataignon <philippe@portable-sre>
2022-12-05 13:26:48 +0100

    05-2

--- 05/01.py
+++ 05/01.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 from parse import parse
 
-def move(s, n, a, b):
+def move1(s, n, a, b):
     for i in range(n):
         c = s[a-1].pop()
         s[b-1].append(c)
     return s
 
+def move2(s, n, a, b):
+    cc = []
+    for i in range(n):
+        cc.insert(0, s[a-1].pop())
+    s[b-1] += cc
+    return s
+
 if __name__ == '__main__':
     f = open("input.txt")
-    # f = open("test.txt")
+    #f = open("test.txt")
     s = {}
     while True:
         l = f.readline()[:-1]
-        print(l)
         cs = [l[x+1:x+2] for x in range(0, len(l), 4)]
         if cs[0] == "1":
             break
@@ -22,15 +28,12 @@ if __name__ == '__main__':
                 s[i] = []
             if c.strip() != "":
                 s[i].insert(0, c)
-    print(s)
     l = f.readline()
     while True:
         l = f.readline()
-        print(l)
         if l == "":
             break
         n, a, b = [int(x) for x in parse("move {} from {} to {}", l)]
-        s = move(s, n, a, b)
-        print(s, n, a, b)
+        s = move2(s, n, a, b)
 
     print("".join([s[x][-1] for x in s]))
