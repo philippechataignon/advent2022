e49b499d9b23a3f526ad2d1649b073c9bf0e04c0
Philippe Chataignon <philippe.chataignon@gmail.com>
2022-12-11 15:31:07 +0100

    08-1

--- 08/01.py
+++ 08/01.py
@@ -3,48 +3,45 @@ import numpy as np
 from parse import parse
 
 def main():
-    f = open("test.txt")
+    f = open("input.txt")
     g = np.array([[int(x) for x in l[:-1]] for l in f], dtype=np.int8)
     print(g)
     print(g.shape)
     v = np.zeros(g.shape, dtype=np.bool)
-    # top
     for c in range(g.shape[1]):
-        for r in range(g.shape[0]):
-            if r == 0:
+        # top
+        r = 0
+        l1 = -1
+        while r < g.shape[0]:
+            if g[r, c] > l1:
                 v[r, c] = True
-            elif g[r - 1, c] < g[r, c]:
+                l1 = g[r, c]
+            r += 1
+        # bottom
+        r = g.shape[0] - 1
+        l2 = -1
+        while r >= 0:
+            if g[r, c] > l2:
                 v[r, c] = True
-            else:
-                break
-    # bottom
-    for c in range(g.shape[1]):
-        for r in reversed(range(g.shape[0])):
-            if r == g.shape[0] - 1:
-                v[r, c] = True
-            elif g[r + 1, c] < g[r, c]:
-                v[r, c] = True
-            else:
-                break
-    # left
+                l2 = g[r, c]
+            r -= 1
     for r in range(g.shape[0]):
-        for c in range(g.shape[1]):
-            if c == 0:
+        # left
+        c = 0
+        l1 = -1
+        while c < g.shape[1]:
+            if g[r, c] > l1:
                 v[r, c] = True
-            elif g[r, c - 1] < g[r, c]:
+                l1 = g[r, c]
+            c += 1
+        # right
+        c = g.shape[1] - 1
+        l2 = -1
+        while c >= 0:
+            if g[r, c] > l2:
                 v[r, c] = True
-            else:
-                break
-    # right
-    for r in range(g.shape[0]):
-        for c in reversed(range(g.shape[1])):
-            if c == g.shape[1] - 1:
-                v[r, c] = True
-            elif g[r, c + 1] < g[r, c]:
-                v[r, c] = True
-            else:
-                break
-
+                l2 = g[r, c]
+            c -= 1
     print(v)
     print(sum(sum(v)))
 
